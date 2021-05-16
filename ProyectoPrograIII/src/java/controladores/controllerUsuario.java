c7b44f281f7f60d00d5688f7050b2ac5868a8a62
Antonio <56178282+Antonio2k19@users.noreply.github.com>
2021-05-16 09:37:16 -0600

    Update controllerUsuario.java
    Controlador para Usuario

--- ProyectoPrograIII/src/java/controladores/controllerUsuario.java
+++ ProyectoPrograIII/src/java/controladores/controllerUsuario.java
@@ -1,15 +1,23 @@
 package controladores;
 
+import dao.DaoRol;
+import dao.DaoUsuario;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import modelos.Rol;
+import modelos.Usuario;
 
 @WebServlet(name = "controllerUsuario", urlPatterns = {"/controllerUsuario"})
 public class controllerUsuario extends HttpServlet {
+    
+    String listar = "Seguridad/Usuario/consultaUsuarios.jsp";
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -35,7 +43,70 @@ public class controllerUsuario extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        //processRequest(request, response);
+        String acceso = "";
+        String action = request.getParameter("accion");
+        DaoUsuario daoUsuario = new DaoUsuario();
+        List<Usuario> lstUsuario = null;
+        DaoRol daoRol = new DaoRol();
+        List<Rol> lstRol = null;
+        Usuario user = new Usuario();
+
+        switch (action) {
+            case "read":
+                lstRol = daoRol.listar();
+                lstUsuario = daoUsuario.listar();
+                request.setAttribute("rol", lstRol);
+                request.setAttribute("user", lstUsuario);
+                acceso = listar;
+                break;
+            case "agregar":
+                user = new Usuario();
+                user.setUser(request.getParameter("Auser"));
+                user.setNombre(request.getParameter("Anombre"));
+                user.setApellido(request.getParameter("Aapellido"));
+                user.setPass(request.getParameter("Apassword"));
+                user.setIdRol(Integer.parseInt(request.getParameter("Arole")));
+                user.setIsActivo(Integer.parseInt(request.getParameter("Aactivo")));
+                
+                daoUsuario.insertar(user);
+                lstRol = daoRol.listar();
+                lstUsuario = daoUsuario.listar();
+                request.setAttribute("rol", lstRol);
+                request.setAttribute("user", lstUsuario);
+                acceso = listar;
+                break;
+            case "editar":
+                user = new Usuario();
+                user.setIdUser(Integer.parseInt(request.getParameter("Eiduser")));
+                user.setUser(request.getParameter("Euser"));
+                user.setNombre(request.getParameter("Enombre"));
+                user.setApellido(request.getParameter("Eapellido"));
+                user.setPass(request.getParameter("Epassword"));
+                user.setIdRol(Integer.parseInt(request.getParameter("Erole")));
+                user.setIsActivo(Integer.parseInt(request.getParameter("Eactivo")));
+                
+                daoUsuario.modificar(user);
+                lstRol = daoRol.listar();
+                lstUsuario = daoUsuario.listar();
+                request.setAttribute("rol", lstRol);
+                request.setAttribute("user", lstUsuario);
+                acceso = listar;
+                break;
+            case "eliminar":
+                user = new Usuario();
+                user.setIdUser(Integer.parseInt(request.getParameter("Diduser")));
+                
+                daoUsuario.eliminar(user);
+                lstRol = daoRol.listar();
+                lstUsuario = daoUsuario.listar();
+                request.setAttribute("rol", lstRol);
+                request.setAttribute("user", lstUsuario);
+                acceso = listar;
+                break;
+        }
+        RequestDispatcher vista = request.getRequestDispatcher(acceso); //invoca de modo directo un recurso web
+        vista.forward(request, response);
     }
     @Override
     public String getServletInfo() {
