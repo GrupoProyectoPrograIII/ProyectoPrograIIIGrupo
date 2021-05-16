c2c48971f8d58b0dc49d22793263d28de4e109d7
Antonio <56178282+Antonio2k19@users.noreply.github.com>
2021-05-16 09:33:18 -0600

    Update controllerRol.java
    Crontrolador para Rol

--- ProyectoPrograIII/src/java/controladores/controllerRol.java
+++ ProyectoPrograIII/src/java/controladores/controllerRol.java
@@ -1,15 +1,21 @@
 package controladores;
 
+import dao.DaoRol;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import modelos.Rol;
 
 @WebServlet(name = "controllerRol", urlPatterns = {"/controllerRol"})
 public class controllerRol extends HttpServlet {
+    
+    String listar = "Seguridad/Roles/consultaRol.jsp";
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -35,7 +41,54 @@ public class controllerRol extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        //processRequest(request, response);
+        String acceso = "";
+        String action = request.getParameter("accion");
+        
+        DaoRol daor = new DaoRol();
+        List<Rol> lstRol = null;
+        Rol rol = new Rol();
+
+        switch (action) {
+            case "read":
+                lstRol = daor.listar();
+                request.setAttribute("rol", lstRol);
+                acceso = listar;
+                break;             
+            case "agregar":
+                rol = new Rol();
+                rol.setNombre(request.getParameter("Anombre"));
+                rol.setDescripcion(request.getParameter("Adescripcion"));
+                rol.setIsActivo(Integer.parseInt(request.getParameter("Aactivo")));
+                daor.insertar(rol);
+                
+                lstRol = daor.listar();
+                request.setAttribute("rol", lstRol);                
+                acceso=listar;
+                break;
+            case "editar":
+                rol = new Rol();
+                rol.setIdRol(Integer.parseInt(request.getParameter("Eidrol")));
+                rol.setNombre(request.getParameter("Enombre"));
+                rol.setDescripcion(request.getParameter("Edescripcion"));
+                rol.setIsActivo(Integer.parseInt(request.getParameter("Eactivo")));
+                daor.modificar(rol);
+                
+                lstRol = daor.listar();
+                request.setAttribute("rol", lstRol);                
+                acceso=listar;
+                break;
+            case "eliminar":
+                rol.setIdRol(Integer.parseInt(request.getParameter("Didrol")));
+                daor.eliminar(rol);
+                
+                lstRol = daor.listar();
+                request.setAttribute("rol", lstRol);                
+                acceso=listar;
+                break;
+        }
+        RequestDispatcher vista = request.getRequestDispatcher(acceso); //invoca de modo directo un recurso web
+        vista.forward(request, response);
     }
     @Override
     public String getServletInfo() {
