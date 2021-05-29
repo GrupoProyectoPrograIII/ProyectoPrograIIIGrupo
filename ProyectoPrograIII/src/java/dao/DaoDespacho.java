f77a7437f73c9e0ac5b9a2085bbb4e615bf02ac1
Antonio <56178282+Antonio2k19@users.noreply.github.com>
2021-05-29 04:20:50 -0600

    Update DaoDespacho.java

--- ProyectoPrograIII/src/java/dao/DaoDespacho.java
+++ ProyectoPrograIII/src/java/dao/DaoDespacho.java
@@ -30,7 +30,24 @@ public class DaoDespacho implements crudDespacho{
 
     @Override
     public Despacho list(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        sql = "SELECT * FROM DESPACHO WHERE ID_DESPACHO="+ id;
+        try {
+            con.open();
+            rs = con.executeQuery(sql);
+            while(rs.next()){
+                despacho.setId_despacho(rs.getInt("ID_DESPACHO"));
+                despacho.setPedido(rs.getInt("PEDIDO"));
+                despacho.setDescripcion(rs.getNString("DESCRIPCION"));
+                despacho.setArea(rs.getString("AREA"));
+                despacho.setMesa(rs.getString("MESA"));                
+            }
+            rs.close();
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return despacho;
     }
 
     @Override
